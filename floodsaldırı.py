407b11a1670f993fe133c87c9ad2a9b711e1f055
boran <boranerdem457@gmail.com>
2026-06-12 15:08:23 +0300

    Update floodsaldırı.py

--- floodsaldırı.py
+++ floodsaldırı.py
@@ -1,13 +1,13 @@
 from scapy.all import IP, ICMP, send
 import time
 
-target_ip = "192.168.1.100"  # Test makinenizin IP'si
+target_ip = "192.168.1.1"  
 
 for i in range(10):
     packet = IP(dst=target_ip) / ICMP()
 
     send(packet, verbose=False)
 
-    print(f"{i+1}. ICMP paketi gönderildi")
+    print(f"{i+1}. ICMP saldırı paketi gönderildi")
 
-    time.sleep(1)  # Saniyede 1 paket
+    time.sleep(1)  
